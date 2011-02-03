7308065583ca0361fcadec8cd6453068d429c704
Ben Corlett <ben@tjstechnology.com.au>
2011-02-03 18:36:15 +1100

    Virtually rewrote entire package. No more modifications of ORM library (allows easy upgrading). magic methods both static now, no more ORM::factory('somemodel'), you must load the model class itself - only way for HMVC - see README.
    Signed-off-by: Ben Corlett <ben@tjstechnology.com.au>

---
+++ classes/db.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Kohana ORM for Fuel.
+ *
+ * @package		Kohana
+ * @category	ORM
+ * @author		TJS Technology Pty Ltd
+ * @link		http://www.tjstechnology.com.au
+ */
+
+namespace ORM;
+
+abstract class DB extends \Fuel\Core\DB
+{
+	
+}
