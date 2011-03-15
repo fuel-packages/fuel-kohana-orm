b2311dd026a8ad97cd9bb0bb519284fd888bb111
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2011-03-15 17:53:13 +0100

    Updated to work with latest Database class changes from Fuel.

--- classes/database.php
+++ classes/database.php
@@ -10,7 +10,11 @@
 
 namespace ORM;
 
-abstract class Database extends \Fuel\Core\Database
+abstract class Database extends \Fuel\Core\Database_Connection
 {
-	
+	// Query types, needed for compatibility with Kohana Database class usage
+	const SELECT =  1;
+	const INSERT =  2;
+	const UPDATE =  3;
+	const DELETE =  4;
 }
