01dc050fc708fa8acf34db9e1a48f318363f51d0
Ben Corlett <bencorlett@me.com>
2011-04-19 23:17:43 +1000

    Added license and docblocks.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * Kohana Orm
+ * 
+ * The Kohana Orm Fuel package is
+ * a port of the Kohana Orm
+ * 
+ * @package		Fuel
+ * @subpackage	Kohana Orm
+ * @version		3.1.2
+ * @author		Ben Corlett
+ * @company		TJS Technology
+ * @website		http://www.tjstechnology.com.au
+ * @license		See LICENSE.md
+ */
 
-Autoloader::add_core_namespace('Kohana');
-
+// Add classes
 Autoloader::add_classes(array(
 	
 	// Fuel wrappers
