aab0fdff23a7b558839021acc833cfe45d9040b7
Ben Corlett <bencorlett@me.com>
2011-04-19 21:17:43 +0800

    Added license and docblocks.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- classes/database.php
+++ classes/database.php
@@ -1,5 +1,18 @@
 <?php
-
+/**
+ * Kohana Orm
+ * 
+ * The Kohana Orm Fuel package is
+ * a port of the Kohana Orm
+ * 
+ * @package		Fuel
+ * @subpackage	Kohana Orm
+ * @version		3.1.2
+ * @author		Ben Corlett
+ * @company		TJS Technology
+ * @website		http://www.tjstechnology.com.au
+ * @license		See LICENSE.md
+ */
 namespace Kohana;
 
 abstract class Database extends \Fuel\Core\Database_Connection {
