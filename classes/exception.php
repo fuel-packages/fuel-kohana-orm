11f9f1c4224a129138e82f257b3fcd23f7b674da
Ben Corlett <bencorlett@me.com>
2011-04-19 22:32:36 +1000

    Continued development. Still need to add Validation.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- classes/exception.php
+++ classes/exception.php
@@ -4,4 +4,53 @@ namespace Kohana;
 
 class Kohana_Exception extends \Fuel\Core\Fuel_Exception {
 	
+	/**
+	 * Creates a new translated exception.
+	 *
+	 *     throw new Kohana_Exception('Something went terrible wrong, :user',
+	 *         array(':user' => $user));
+	 *
+	 * @access	public
+	 * @param   string     error message
+	 * @param   array      translation variables
+	 * @param   integer    the exception code
+	 * @return  void
+	 */
+	public function __construct($message, array $variables = NULL, $code = 0)
+	{
+		// Set the message
+		$message = empty($variables) ? $message : strtr($message, $variables);
+
+		// Pass the message to the parent
+		parent::__construct($message, $code);
+	}
+
+	/**
+	 * Magic object-to-string method.
+	 *
+	 *     echo $exception;
+	 * 
+	 * @access	public
+	 * @uses    Kohana::exception_text
+	 * @return  string
+	 */
+	public function __toString()
+	{
+		return $this->exception_text($this);
+	}
+	
+	/**
+	 * Get a single line of text representing the exception:
+	 *
+	 * Error [ Code ]: Message ~ File [ Line ]
+	 * 
+	 * @access	public
+	 * @param   object  Exception
+	 * @return  string
+	 */
+	public function exception_text(Exception $e)
+	{
+		return sprintf('%s [ %s ]: %s ~ %s [ %d ]',
+			get_class($e), $e->getCode(), strip_tags($e->getMessage()), Kohana::debug_path($e->getFile()), $e->getLine());
+	}
 }
