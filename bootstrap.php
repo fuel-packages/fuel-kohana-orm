0c3fca7b567c63ed19583c7ce9ce51ab16d90b48
Ben Corlett <bencorlett@me.com>
2011-04-19 19:19:12 +0800

    Started rewrite including Kohana ORM from Kohana 3.1.2 (currently the package uses the Kohana ORM from Kohana 3.0.9)
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- bootstrap.php
+++ bootstrap.php
@@ -1,22 +1,17 @@
 <?php
-/**
- * Kohana ORM for Fuel.
- *
- * @package		Kohana
- * @category	ORM
- * @author		TJS Technology Pty Ltd
- * @link		http://www.tjstechnology.com.au
- */
 
 Autoloader::add_core_namespace('Kohana');
 
 Autoloader::add_classes(array(
-	'Kohana\\Database'				=> __DIR__ . '/classes/database.php',
-	'Kohana\\DB'					=> __DIR__ . '/classes/db.php',
-	'Kohana\\Inflector'				=> __DIR__ . '/classes/inflector.php',
-	'Kohana\\Kohana_ORM'			=> __DIR__ . '/classes/kohana/orm.php',
-	'Kohana\\Kohana_Exception'		=> __DIR__ . '/classes/kohana/exception.php',
-	'Kohana\\Orm'					=> __DIR__ . '/classes/orm.php',
+	
+	// Fuel wrappers
+	'Kohana\\Kohana_Exception'					=> __DIR__ . '/classes/exception.php',
+	'Kohana\\Model'								=> __DIR__ . '/classes/model.php',
+	'Kohana\\Orm'       						=> __DIR__ . '/classes/orm.php',
+	'Kohana\\Validation'						=> __DIR__ . '/classes/validation.php',
+	
+	// Original Kohana classes
+	'Kohana\\Kohana_Model'						=> __DIR__ . '/vendor/kohana/model.php',
+	'Kohana\\Kohana_ORM'						=> __DIR__ . '/vendor/kohana/orm.php',
+	'Kohana\\Kohana_ORM_Validation_Exception'	=> __DIR__ . '/vendor/kohana/orm/validation/exception.php',
 ));
-
-/* End of file bootstrap.php */
--- classes/inflector.php
+++ classes/inflector.php
@@ -1,24 +1,8 @@
 <?php
-/**
- * Kohana ORM for Fuel.
- *
- * @package		Kohana
- * @category	ORM
- * @author		TJS Technology Pty Ltd
- * @link		http://www.tjstechnology.com.au
- */
 
 namespace Kohana;
 
-class Inflector extends \Fuel\Core\Inflector
-{
-	public static function plural($word)
-	{
-		return \Fuel\Core\Inflector::pluralize($word);
-	}
+class Inflector extends \Fuel\Core\Inflector {
 	
-	public static function singular($word)
-	{
-		return \Fuel\Core\Inflector::singularize($word);
-	}
+	
 }
